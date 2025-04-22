2096b3cf06c0ca2395e1973539689d1584cd3e73
pachecoleoo <leomasa@live.com.ar>
2025-04-22 16:31:45 -0300

    pruebo algo

--- src/components/selectFilter/SelectFilter.jsx
+++ src/components/selectFilter/SelectFilter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from './SelectFilter.module.css';
 function SelectFilter({ nombre, options, valor, onChange }) {
   const optionsList = options || []
   return (
